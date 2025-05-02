91411d4b888e7ece60aca4520752f2b96fb3a001
DevanshParashar165 <parashardevansh165@gmail.com>
2025-05-02 20:22:40 +0530

    Utils code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import mongoose from "mongoose";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT} `)
+    })
+    app.on("error", (error) => {
+        console.log("Error : ", error);
+        throw error
+    })
+}) 
+.catch((error)=>{
+    console.log("MongoDB connection Failed !!! ",error)
+})
 
 
 /* One more way to connect database 
